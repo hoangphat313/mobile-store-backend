9b81e07780339fccc21f91f5b46a045cf39aa698
hoangphat313 <phoangphat313@gmail.com>
2024-07-12 00:19:16 +0700

    final

--- src/routes/ProductRouter.js
+++ src/routes/ProductRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const ProductController = require('../controller/ProductController');
+const ProductController = require("../controller/ProductController");
 const { authMiddleware } = require("../middleware/authMiddleware");
 
 router.post('/create', ProductController.createProduct)
