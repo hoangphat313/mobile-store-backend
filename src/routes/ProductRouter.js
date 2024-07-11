3589ac278407aa201d08cb6ce16a8db913a9fab1
hoangphat313 <phoangphat313@gmail.com>
2024-07-12 00:36:38 +0700

    final router

--- src/routes/ProductRouter.js
+++ src/routes/ProductRouter.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const ProductController = require("../controller/ProductController");
-const { authMiddleware } = require("../middleware/authMiddleware");
+const ProductController = require("../controller/ProductController")
+const { authMiddleWare } = require("../middleware/authMiddleware");
 
 router.post('/create', ProductController.createProduct)
 router.put('/update/:id', ProductController.updateProduct)
 router.get('/get-all', ProductController.getAllProduct)
 router.get('/get-all-type', ProductController.getAllType)
 //
-router.post('/delete-many', authMiddleware, ProductController.deleteMany) //authMiddleWare
-router.get('/get-details/:id', authMiddleware, ProductController.getDetailsProduct) //,authMiddleware
-router.delete('/delete/:id', authMiddleware, ProductController.deleteProduct)//,authMiddleware
+router.post('/delete-many', authMiddleWare, ProductController.deleteMany) //authMiddleWare
+router.get('/get-details/:id', authMiddleWare, ProductController.getDetailsProduct) //,authMiddleware
+router.delete('/delete/:id', authMiddleWare, ProductController.deleteProduct)//,authMiddleware
+
 module.exports = router 
