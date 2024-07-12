594242439533e7304d4c81d84e8bcaac1a76586a
hoangphat313 <phoangphat313@gmail.com>
2024-07-13 00:22:53 +0700

    fix access token

--- src/services/JwtService.js
+++ src/services/JwtService.js
@@ -5,7 +5,7 @@ dotenv.config()
 
 const generalAccessToken = async (payload) => {
 
-    const access_token = jwt.sign(payload, process.env.ACCESS_TOKEN, { expiresIn: '30s' })
+    const access_token = jwt.sign(payload, process.env.ACCESS_TOKEN, { expiresIn: '15m' })
     return access_token
 }
 const generalRefreshToken = async (payload) => {
