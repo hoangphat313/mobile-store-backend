a64399b134989403d11ab28f175475c9ce958b28
hoangphat313 <phoangphat313@gmail.com>
2024-07-12 08:45:05 +0700

    final router

--- src/routes/UserRouter.js
+++ src/routes/UserRouter.js
@@ -7,8 +7,8 @@ router.post('/sign-up', userController.createUser)
 router.post('/sign-in', userController.loginUser)
 router.post('/log-out', userController.logoutUser)
 router.post('/refresh-token', userController.refreshToken)
+router.put('/update-user/:id', userController.updateUser)
 //
-router.put('/update-user/:id', authUserMiddleWare, userController.updateUser) //,authUserMiddleware
 router.get('/get-details/:id', authUserMiddleWare, userController.getDetailsUser) //,authUserMiddleware
 //
 router.delete('/delete-user/:id', authMiddleWare, userController.deleteUser) //authMiddleware
